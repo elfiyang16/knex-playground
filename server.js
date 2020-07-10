68ae779f35508011e5ed83e8728c88f66d7f7455
Elfi Yang <elfiyang16@gmail.com>
2020-07-10 13:36:14 +0100

    feat: set up express server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("./knexfile")[environment];
+const database = require("knex")(configuration);
+
+app.use(bodyParser.json());
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (request, response) => {
+  response.send("Hello, Publications!");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`App is running on ${app.get("port")}.`);
+});
+
+module.exports = app;
